f0aded5a32a33b745a1a62252b781e34dee2f5f6
pavankandukuri <54271700+pavankandukuri@users.noreply.github.com>
2023-11-23 08:07:50 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+# Create an instance of the Flask class
+app = Flask(__name__)
+
+# Define a route for the default URL
+@app.route('/')
+def home():
+    # Render the 'index.html' template
+    return "This is my webapp"
+
+# Run the application if the script is executed
+if __name__ == '__main__':
+    # Set debug=True to enable auto-reloading of the server on code changes
+    app.run()
